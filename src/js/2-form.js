cb4ee83054a99b863682662bb517b6d0db01a5b0
Iryna Yermak <1991irenka@gmail.com>
2025-09-05 14:17:40 +0100

    made hw-9

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+const formDataKey = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const savedMessage = localStorage.getItem(formDataKey);
+
+if (savedMessage) {
+  const parsed = JSON.parse(savedMessage);
+  form.elements.email.value = parsed.email;
+  form.elements.message.value = parsed.message;
+  formData.email = parsed.email;
+  formData.message = parsed.message;
+}
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+  formData.email = form.elements.email.value.trim();
+  formData.message = form.elements.message.value.trim();
+  localStorage.setItem(formDataKey, JSON.stringify(formData));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(formDataKey);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+}
